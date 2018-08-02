4966c94c332546dc66ea2764fc55c29bae425c67
gwinbleidd <vspotapov@gmail.com>
2018-08-02 16:58:44 +0300

    Some changes

--- test/emcli_test.py
+++ test/emcli_test.py
@@ -3,16 +3,26 @@
 
 import sys
 import os
+import logging
+
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, 'lib'))
 
 from emcli import *
 
-start = datetime.datetime.now()
 
-emcli = Emcli()
+def main():
+    log_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, 'log', 'test.log')
+    logging.basicConfig(filename=log_filename, level=logging.INFO,
+                        format="%(asctime)s - %(process)d - %(levelname)s - %(message)s")
 
-print emcli.get_event_id('71E4097171ECBA19E0534539780ACF99')
+    start = datetime.datetime.now()
+    try:
+        emcli = Emcli()
+        print emcli.get_event_id('71E4097171ECBA19E0534539780ACF99')
+        logging.info('Tiem spent: %s' % str(datetime.datetime.now() - start))
+    except Exception as e:
+        logging.error(e, exc_info=True)
 
-# print emcli.configured
 
-print datetime.datetime.now() - start
+if __name__ == "__main__":
+    main()
